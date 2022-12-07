b3ded6a210045d5038a7839f53aea34b994f6836
Kevin Liu <kliu33@buffalo.edu>
2022-12-07 11:56:56 -0500

    finished import.db

--- import_db.sql
+++ import_db.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -21,7 +23,10 @@ DROP TABLE IF EXISTS question_follows;
 
 CREATE TABLE question_follows (
     question_id INTEGER NOT NULL,
-    user_id INTEGER NOT NULL
+    user_id INTEGER NOT NULL,
+
+    FOREIGN KEY (question_id) REFERENCES questions(question_id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 DROP TABLE IF EXISTS replies;
@@ -30,7 +35,7 @@ CREATE TABLE replies (
     replies_id INTEGER PRIMARY KEY,
     user_id INTEGER NOT NULL,
     question_id INTEGER NOT NULL,
-    parent_reply_id INTEGER NOT NULL,
+    parent_reply_id INTEGER,
     body TEXT NOT NULL,
 
     FOREIGN KEY (question_id) REFERENCES questions(question_id),
@@ -38,5 +43,25 @@ CREATE TABLE replies (
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+DROP TABLE IF EXISTS question_likes;
 
+CREATE TABLE question_likes (
+    like_id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
 
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (question_id) REFERENCES questions(question_id)
+)       
+
+INSERT INTO
+    users (fname, lname)
+VALUES  
+    ('Brandon', 'Choi'),
+    ('Kevin', 'Liu');
+
+INSERT INTO
+    questions (title, body, author_id)
+VALUES
+    ('NOT NULL?', 'Why do we need NOT NULL in sql?', (SELECT id FROM users WHERE fname = 'Brandon' AND lname = 'Choi')),
+    ('Caps?', 'All sql in caps?', (SELECT id FROM users WHERE fname = 'Kevin' AND lname = 'Liu'));
